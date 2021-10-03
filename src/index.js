13588f0870987c0baf88ac7910363c90630bd032
Philipp Stampfer <philipp.stampfer@protonmail.ch>
2021-10-04 00:59:13 +0200

    feat(index): spawn pipes with distance

--- src/index.js
+++ src/index.js
@@ -25,21 +25,23 @@ const config = {
 const VELOCITY_X = 20;
 const VELOCITY_Y = 200;
 
-const FLAP_VELOCITY = 175;
+const PIPES_TO_RENDER = 4;
+
+const FLAP_VELOCITY_Y = 175;
+// const FLAP_VELOCITY_X = 20;
 
 const INITIAL_PLAYER_POSITION = { x: config.width / 10, y: config.height / 2 };
 const PIPE_VERTICCALDISTANCE_RANGE = [150, 250];
 
 let player = null;
 
-let upperPipe = null;
-let lowerPipe = null;
+let pipeHorizontalDistance = 0;
 
-let pipeVerticalDistance = Phaser.Math.Between(...PIPE_VERTICCALDISTANCE_RANGE);
-// todo: try to resolve this --> calc not possible in Between()!!
-let tmp = [0 + 20, config.height - 20 - pipeVerticalDistance];
+// let pipeVerticalDistance = Phaser.Math.Between(...PIPE_VERTICCALDISTANCE_RANGE);
+// // todo: try to resolve this --> calc not possible in Between()!!
+// let tmp = [0 + 20, config.height - 20 - pipeVerticalDistance];
 
-let pipeVerticalPosition = 0; pipeVerticalPosition = Phaser.Math.Between(...tmp);
+// let pipeVerticalPosition = 0; pipeVerticalPosition = Phaser.Math.Between(...tmp);
 
 let totalDelta = null;
 
@@ -51,12 +53,22 @@ function preload() {
 
 function create() {
   this.add.image(0, 0, 'sky').setOrigin(0);
+
   // call it on the physics object - so the bird has a body
   player = this.physics.add.sprite(INITIAL_PLAYER_POSITION.x, INITIAL_PLAYER_POSITION.y, 'bird').setOrigin();
   player.body.gravity.y = 400;
 
-  upperPipe = this.physics.add.sprite(400, pipeVerticalPosition, 'pipe').setOrigin(0, 1);
-  lowerPipe = this.physics.add.sprite(400, upperPipe.y + pipeVerticalDistance, 'pipe').setOrigin(0, 0);
+  for (let i = 0; i < PIPES_TO_RENDER; i++) {
+
+    let upperPipe = this.physics.add.sprite(0, 0, 'pipe').setOrigin(0, 1);
+    let lowerPipe = this.physics.add.sprite(0, 0, 'pipe').setOrigin(0, 0);
+    initPipes(upperPipe, lowerPipe);
+  }
+
+
+
+  // upperPipe = this.physics.add.sprite(400, pipeVerticalPosition, 'pipe').setOrigin(0, 1);
+  // lowerPipe = this.physics.add.sprite(400, upperPipe.y + pipeVerticalDistance, 'pipe').setOrigin(0, 0);
 
   this.input.on('pointerdown', flap);
   this.input.keyboard.on('keydown-SPACE', flap);
@@ -70,6 +82,23 @@ function update(time, delta) {
   }
 }
 
+function initPipes(uPipe, lPipe) {
+  pipeHorizontalDistance += 400;
+  let pipeVerticalDistance = Phaser.Math.Between(...PIPE_VERTICCALDISTANCE_RANGE);
+  // todo: try to resolve this --> calc not possible in Between()!!
+  let tmp = [0 + 20, config.height - 20 - pipeVerticalDistance];
+  let pipeVerticalPosition = Phaser.Math.Between(...tmp);
+
+  uPipe.x = pipeHorizontalDistance;
+  uPipe.y = pipeVerticalPosition;
+
+  lPipe.x = uPipe.x;
+  lPipe.y = uPipe.y + pipeVerticalPosition;
+
+  lPipe.body.velocity.x = -200;
+  uPipe.body.velocity.x = -200;
+}
+
 function resetPlayerPosition() {
   player.x = INITIAL_PLAYER_POSITION.x;
   player.y = INITIAL_PLAYER_POSITION.y;
@@ -77,8 +106,8 @@ function resetPlayerPosition() {
 }
 
 function flap() {
-  player.body.velocity.y = -FLAP_VELOCITY;
-  player.body.velocity.x = VELOCITY_X;
+  player.body.velocity.y = -FLAP_VELOCITY_Y;
+  // player.body.velocity.x = FLAP_VELOCITY_X;
 }
 
 new Phaser.Game(config);
