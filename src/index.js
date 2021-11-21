a7af0b13ee60b0925a91682ceb3ca7ad69b7941a
philipp.stampfer <philipp.stampfer@protonmail.ch>
2021-11-21 20:32:54 +0100

    feat(game): refactoring game

--- src/index.js
+++ src/index.js
@@ -1,113 +1,37 @@
-import Phaser from "phaser";
+
+import Phaser from 'phaser';
+
+import PlayScene from './scenes/PlayScene';
+import MenuScene from './scenes/MenuScene';
+import PreloadScene from './scenes/PreloadScene';
+import ScoreScene from './scenes/ScoreScene';
+import PauseScene from './scenes/PauseScene';
+
+const WIDTH = 400;
+const HEIGHT = 600;
+const BIRD_POSITION = { x: WIDTH * 0.1, y: HEIGHT / 2 };
+
+const SHARED_CONFIG = {
+  width: WIDTH,
+  height: HEIGHT,
+  startPosition: BIRD_POSITION
+}
+
+const Scenes = [PreloadScene, MenuScene, ScoreScene, PlayScene, PauseScene];
+const createScene = Scene => new Scene(SHARED_CONFIG)
+const initScenes = () => Scenes.map(createScene)
 
 const config = {
-  // WebGL 
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  ...SHARED_CONFIG,
+  pixelArt: true,
   physics: {
-    // Arcade physics -- > manages physics simulation
     default: 'arcade',
     arcade: {
-      debug: true,
-      gravity: {
-        // y: 400
-      }
+      // debug: true,
     }
   },
-  scene: {
-    preload: preload,
-    create: create,
-    update: update
-  }
-}
-
-const VELOCITY_X = 20;
-const VELOCITY_Y = 200;
-
-const PIPES_TO_RENDER = 4;
-
-const FLAP_VELOCITY_Y = 175;
-// const FLAP_VELOCITY_X = 20;
-
-const INITIAL_PLAYER_POSITION = { x: config.width / 10, y: config.height / 2 };
-const PIPE_VERTICCALDISTANCE_RANGE = [150, 250];
-
-let player = null;
-
-let pipeHorizontalDistance = 0;
-
-// let pipeVerticalDistance = Phaser.Math.Between(...PIPE_VERTICCALDISTANCE_RANGE);
-// // todo: try to resolve this --> calc not possible in Between()!!
-// let tmp = [0 + 20, config.height - 20 - pipeVerticalDistance];
-
-// let pipeVerticalPosition = 0; pipeVerticalPosition = Phaser.Math.Between(...tmp);
-
-let totalDelta = null;
-
-function preload() {
-  this.load.image('sky', 'assets/sky.png');
-  this.load.image('bird', 'assets/bird.png');
-  this.load.image('pipe', 'assets/pipe.png');
-}
-
-function create() {
-  this.add.image(0, 0, 'sky').setOrigin(0);
-
-  // call it on the physics object - so the bird has a body
-  player = this.physics.add.sprite(INITIAL_PLAYER_POSITION.x, INITIAL_PLAYER_POSITION.y, 'bird').setOrigin();
-  player.body.gravity.y = 400;
-
-  for (let i = 0; i < PIPES_TO_RENDER; i++) {
-
-    let upperPipe = this.physics.add.sprite(0, 0, 'pipe').setOrigin(0, 1);
-    let lowerPipe = this.physics.add.sprite(0, 0, 'pipe').setOrigin(0, 0);
-    initPipes(upperPipe, lowerPipe);
-  }
-
-
-
-  // upperPipe = this.physics.add.sprite(400, pipeVerticalPosition, 'pipe').setOrigin(0, 1);
-  // lowerPipe = this.physics.add.sprite(400, upperPipe.y + pipeVerticalDistance, 'pipe').setOrigin(0, 0);
-
-  this.input.on('pointerdown', flap);
-  this.input.keyboard.on('keydown-SPACE', flap);
-}
-
-
-//
-function update(time, delta) {
-  if (player.y > config.height || player.y < -player.height) {
-    resetPlayerPosition();
-  }
-}
-
-function initPipes(uPipe, lPipe) {
-  pipeHorizontalDistance += 400;
-  let pipeVerticalDistance = Phaser.Math.Between(...PIPE_VERTICCALDISTANCE_RANGE);
-  // todo: try to resolve this --> calc not possible in Between()!!
-  let tmp = [0 + 20, config.height - 20 - pipeVerticalDistance];
-  let pipeVerticalPosition = Phaser.Math.Between(...tmp);
-
-  uPipe.x = pipeHorizontalDistance;
-  uPipe.y = pipeVerticalPosition;
-
-  lPipe.x = uPipe.x;
-  lPipe.y = uPipe.y + pipeVerticalPosition;
-
-  lPipe.body.velocity.x = -200;
-  uPipe.body.velocity.x = -200;
-}
-
-function resetPlayerPosition() {
-  player.x = INITIAL_PLAYER_POSITION.x;
-  player.y = INITIAL_PLAYER_POSITION.y;
-  player.body.velocity.y = 0;
-}
-
-function flap() {
-  player.body.velocity.y = -FLAP_VELOCITY_Y;
-  // player.body.velocity.x = FLAP_VELOCITY_X;
+  scene: initScenes()
 }
 
 new Phaser.Game(config);
